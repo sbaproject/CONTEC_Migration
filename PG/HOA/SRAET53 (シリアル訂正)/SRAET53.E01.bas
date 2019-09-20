cefbfa65b7ef887441c150ad5842c83a2896c6f0
hoasba <ndhoa@starboardasia.com>
2019-09-20 17:38:02 +0700

    add SRAET53

---
+++ PG/HOA/SRAET53 (シリアル訂正)/SRAET53.E01.bas
@@ -0,0 +1,6 @@
+Attribute VB_Name = "SRAET53_E01"
+Option Explicit
+
+Sub INITDSP()
+End Sub
+
---
+++ PG/HOA/SRAET53 (シリアル訂正)/SRAET53.NET/SRAET53.E01.vb
@@ -0,0 +1,7 @@
+Option Strict Off
+Option Explicit On
+Module SRAET53_E01
+	
+	Sub INITDSP()
+	End Sub
+End Module
