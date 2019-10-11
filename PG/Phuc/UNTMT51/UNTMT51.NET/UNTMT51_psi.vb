1c7a6fc1079285c97a925490c9d1a1c07417d287
Phuc Nguyen <nhphuc@starboardasia.com>
2019-10-11 13:37:21 +0700

    UNTMT51 - Finish 100%

---
+++ PG/Phuc/UNTMT51/UNTMT51.NET/UNTMT51_psi.vb
@@ -0,0 +1,17 @@
+Option Strict Off
+Option Explicit On
+Module SSSMAIN0002
+	'プログラム総括情報プロシジャ
+	
+	Sub AE_PSIR_SSSMAIN() 'Generated.
+		ReDim AE_PSI(6)
+		AE_PSIC = 7
+		AE_PSI(0) = "HD_OPEID 0000 code 6 - A L N 0 000000 - 1 -"
+		AE_PSI(1) = "HD_OPENM 0000 name 20 - A L N N - - 1 -"
+		AE_PSI(2) = "BD_UNTCD 3303 code 2 - A R N 0 00 - 1 -"
+		AE_PSI(3) = "BD_UNTNM 2202 name 4 - A L N N - - 1 -"
+		AE_PSI(4) = "BD_UPDKB 0000 code 4 - A L N A - - 1 -"
+		AE_PSI(5) = "BV_V_DATKB 0000 code 1 - A L N N - - 1 -"
+		AE_PSI(6) = "BV_V_UNTNM 0000 name 4 - A L N N - - 1 -"
+	End Sub
+End Module
